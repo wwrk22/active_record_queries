4a25ac1d072aa599ac26b56ef0be025ee49d1019
Won Rhim <wwrk22@gmail.com>
2023-01-11 22:42:27 -0500

    Add associations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ApplicationRecord
   belongs_to :supplier
   belongs_to :author
+  has_many :reviews
+  has_and_belongs_to_many :orders, join_table: 'books_orders'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_12_023814) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_12_033858) do
   create_table "authors", force: :cascade do |t|
     t.text "first_name"
     t.text "last_name"
@@ -33,6 +33,43 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_12_023814) do
     t.index ["supplier_id"], name: "index_books_on_supplier_id"
   end
 
+  create_table "customers", force: :cascade do |t|
+    t.text "first_name"
+    t.text "last_name"
+    t.text "email"
+    t.integer "visits"
+    t.integer "orders_count"
+    t.integer "lock_version"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.time "date_submitted"
+    t.integer "status"
+    t.decimal "subtotal"
+    t.decimal "shipping"
+    t.decimal "tax"
+    t.decimal "total"
+    t.integer "customer_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id"], name: "index_orders_on_customer_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "rating"
+    t.integer "state"
+    t.integer "customer_id", null: false
+    t.integer "book_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_reviews_on_book_id"
+    t.index ["customer_id"], name: "index_reviews_on_customer_id"
+  end
+
   create_table "suppliers", force: :cascade do |t|
     t.text "name"
     t.datetime "created_at", null: false
@@ -41,4 +78,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_12_023814) do
 
   add_foreign_key "books", "authors"
   add_foreign_key "books", "suppliers"
+  add_foreign_key "orders", "customers"
+  add_foreign_key "reviews", "books"
+  add_foreign_key "reviews", "customers"
 end
